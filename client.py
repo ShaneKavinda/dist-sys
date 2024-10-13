855d7e927435b70d24df18e5b5cd58e806eec010
ShaneKavinda <shanekavinda1999@gmail.com>
2024-10-13 15:20:05 +0530

    commment for client side app

--- client.py
+++ client.py
@@ -14,15 +14,6 @@ def splashScreen():
     print("-------------------------------------")
     print("")
 
-
-def checkStudent():
-    while True:
-        selection = input("Are you a current EOU Student? (y/n): ").lower()
-        if selection == "y":
-            return True
-        elif selection == "n":
-            return False
-
 def printAuthentication(auth):
     print()
     print("--------your personal details are as below---------------------------")
@@ -39,23 +30,52 @@ def printAuthentication(auth):
         #print (  i,  unit_title,  unit_mark)
         i +=1
 
+# Get user input from the user
+def getUserInput():
+    try:
+        TFN_input = input("Please enter if you have a TFN number (y/n/):")
+        if (TFN_input == "y" or TFN_input == "Y"):
+            get_TFN = True
+        elif (TFN_input == "n" or TFN_input == "N"):
+            get_TFN = False
+        else:
+            raise Exception()
+        
+        #Get the personId if the user has a TFN number
+        if (get_TFN):
+            #Get the personId as user input
+            # Validate if the length of personId = 6 and is an integer
+
+            #Get email address of the user (for authentication)
+
+        #The user does not have a TFN number
+        else:
+            # Prompt user to enter personID
+            # Prompt the user to enter net_wage, tax withheld as a key-value pair (use a loop which the user can exit anytime, with a max range of 26)
+            
+    except Exception:
+        print("Invalid input, please try again")
+        getUserInput()
+        
+
 def main():
     uri = "PYRO:server1@"+SERVER+":"+str(PORT)
     server1 = Pyro4.Proxy(uri)
 
     splashScreen()
 
-    personId = "1V"
-    Email = "testuser@email.com"
+    getUserInput()
+    # personId = "1V"
+    # Email = "testuser@email.com"
     
-    print("Requesting Data from Server....")
-    authentication = server1.getUserDetails(personId, Email)
+    # print("Requesting Data from Server....")
+    # authentication = server1.getUserDetails(personId, Email)
     
-    if not authentication:
-        print("No user details found.")
-        return
+    # if not authentication:
+    #     print("No user details found.")
+    #     return
 
-    printAuthentication(authentication)
+    # printAuthentication(authentication)
 
     
     print("")
