ec47d8a6f02b13b3d09bd67bcac1b2ed35328521
thekabi19 <bkabishanth19@gmail.com>
2024-10-14 21:24:36 +0530

    Test Commit

--- client.py
+++ client.py
@@ -30,6 +30,13 @@ def printAuthentication(auth):
         #print (  i,  unit_title,  unit_mark)
         i +=1
 
+def checkDataInput(ID):
+    if len(ID) != 6:
+        raise Exception("The Entered value should be 6 digit")
+    elif not ID.isdigit():
+        raise Exception ("THe Entered value is not an integer")
+
+
 # Get user input from the user
 def getUserInput():
     try:
@@ -48,10 +55,7 @@ def getUserInput():
             #Get the personId as user input
             # Validate if the length of personId = 6 and is an integer
             person_ID = input("Please Enter Your 6 - Digit Person ID: ").strip()
-            if len(person_ID) != 6:
-                raise Exception("The Entered value should be 6 digit")
-            elif not person_ID.isdigit():
-                raise Exception ("THe Entered value is not an integer")
+            checkIDerror = checkDataInput(person_ID)
             
             #Get email address of the user (for authentication)
             email = input("Enter your email address: ").strip()
@@ -69,10 +73,7 @@ def getUserInput():
             #Prompt user to enter personID
             # Prompt the user to enter net_wage, tax withheld as a key-value pair (use a loop which the user can exit anytime, with a max range of 26)
             person_ID = input("Please Enter Your 6 - Digit Person ID: ").strip()
-            if len(person_ID) != 6:
-                raise Exception("The Entered value should be 6 digit")
-            elif not person_ID.isdigit():
-                raise Exception ("THe Entered value is not an integer")
+            checkIDerror = checkDataInput(person_ID)
             
                # Initialize lists for net wages and tax withheld
             net_wages = []
@@ -98,12 +99,21 @@ def getUserInput():
                 net_wages.append(netWage)
                 tax_withheld.append(taxWithheld)
 
+            #Ask user whether they have Private Health Insurance Cover (PHIC)
+            phic = input("Please state whether you have a Private Health Insurance Cover (PHIC) (y/n/):")
+            if (phic == "y" or phic == "Y"):
+                hasPHIC = True
+            elif (phic == "n" or phic == "N"):
+                hasPHIC = False
+            else:
+                raise Exception()
+            
             #Save User Data
             user_data["person_id"] = person_ID
             user_data["net_wages"] = net_wages
             user_data["tax_withheld"] = tax_withheld
             user_data["has_tfn"] = False
-
+            user_data["has_phic"] = hasPHIC
             return user_data
 
             
@@ -113,8 +123,8 @@ def getUserInput():
         
 
 def main():
-    #uri = "PYRO:server1@"+SERVER+":"+str(PORT)
-    #server1 = Pyro4.Proxy(uri)
+    uri = "PYRO:server1@"+SERVER+":"+str(PORT)
+    server1 = Pyro4.Proxy(uri)
 
     splashScreen()
 
@@ -133,6 +143,18 @@ def main():
     #     return
 
     # printAuthentication(authentication)
+    
+    if user_data["has_tfn"]:
+        print("Requesting Data from Server....")
+        authentication = server1.getUserDetails(user_data["person_id"], user_data["email"])
+
+        if not authentication:
+            print("No user details found.")
+            return
+        
+        printAuthentication(authentication)
+
+    result = server1.processTaxEstimate(user_data["person_ID"], user_data["net_wages"], user_data["tax_withheld"], user_data["has_phic"])
 
     
     print("")
