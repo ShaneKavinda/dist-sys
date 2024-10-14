7f4cd8c774c49c91a8f490009072c85a718dca02
ShaneKavinda <shanekavinda1999@gmail.com>
2024-10-14 20:19:15 +0530

    added input validation in client.py

--- client.py
+++ client.py
@@ -41,30 +41,87 @@ def getUserInput():
         else:
             raise Exception()
         
+        user_data = {}
+
         #Get the personId if the user has a TFN number
         if (get_TFN):
             #Get the personId as user input
             # Validate if the length of personId = 6 and is an integer
-
+            person_ID = input("Please Enter Your 6 - Digit Person ID: ").strip()
+            if len(person_ID) != 6:
+                raise Exception("The Entered value should be 6 digit")
+            elif not person_ID.isdigit():
+                raise Exception ("THe Entered value is not an integer")
+            
             #Get email address of the user (for authentication)
+            email = input("Enter your email address: ").strip()
+            if "@" not in email or "." not in email:
+                raise Exception("Invalid email address. Please enter a valid email.")    
+            
+            user_data ["person_ID"] = person_ID
+            user_data ["email"] = email
+            user_data ["has_TFN"] = True
+
+            return user_data    
 
         #The user does not have a TFN number
         else:
-            # Prompt user to enter personID
+            #Prompt user to enter personID
             # Prompt the user to enter net_wage, tax withheld as a key-value pair (use a loop which the user can exit anytime, with a max range of 26)
+            person_ID = input("Please Enter Your 6 - Digit Person ID: ").strip()
+            if len(person_ID) != 6:
+                raise Exception("The Entered value should be 6 digit")
+            elif not person_ID.isdigit():
+                raise Exception ("THe Entered value is not an integer")
+            
+               # Initialize lists for net wages and tax withheld
+            net_wages = []
+            tax_withheld = []
+
+            print("Please Enter your Bi-Weekly Net Wage and the corresponding Tax Withheld.")
+            for i in range(26):
+                netWage = float(input("Please Enter Your Bi Weekly Net Wage, ENTER -1 to finish entering: "))
+                if netWage == -1:
+                    break
+                elif netWage < 0:
+                    print("Net Wage must be more than 0")
+                    continue
+
+                taxWithheld = float (input ("Please Enter the corresponding Tax Withheld: "))
+                if taxWithheld < 0:
+                    print("Tax withheld cannot be less than 0")
+                    continue
+                elif taxWithheld > netWage:
+                    print ("Tax Withheld cannot be more that Net_wage")
+                    continue
+                
+                net_wages.append(netWage)
+                tax_withheld.append(taxWithheld)
+
+            #Save User Data
+            user_data["person_id"] = person_ID
+            user_data["net_wages"] = net_wages
+            user_data["tax_withheld"] = tax_withheld
+            user_data["has_tfn"] = False
+
+            return user_data
+
             
     except Exception:
         print("Invalid input, please try again")
-        getUserInput()
+        return getUserInput()
         
 
 def main():
-    uri = "PYRO:server1@"+SERVER+":"+str(PORT)
-    server1 = Pyro4.Proxy(uri)
+    #uri = "PYRO:server1@"+SERVER+":"+str(PORT)
+    #server1 = Pyro4.Proxy(uri)
 
     splashScreen()
 
-    getUserInput()
+    user_data = getUserInput()
+    print("USER INFORMATION: ")
+    print (user_data)
+
     # personId = "1V"
     # Email = "testuser@email.com"
     
