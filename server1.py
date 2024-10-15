6c0fe22500781e2425e12ddacaee49ada7fe517a
thekabi19 <bkabishanth19@gmail.com>
2024-10-15 18:35:40 +0530

    Tax calculations

--- server1.py
+++ server1.py
@@ -39,7 +39,44 @@ class server1(object):
         print("in Server1: SA1 -> Client : Sending data back to client")
         return userDetails
 
-    
+    def calculateTaxEstimate(self, personId, net_wages, tax_withheld, taxable_income, has_phic):
+        print(f"Processing tax return estimate for Person ID: {personId}")
+        # Calculate total wages and tax withheld
+        net_income=sum(net_wages)
+        total_tax_withheld = sum(tax_withheld)
+
+        # Simplified tax return logic
+        if taxable_income <= 18200:
+            tax_due = 0
+        elif taxable_income >=18201 and taxable_income <= 45000:
+            tax_due = 0.19 * (taxable_income - 18200)
+        elif taxable_income >= 45001 and  taxable_income <= 120000:
+            tax_due = 5092 + 0.325 * (taxable_income - 45000)
+        else:
+            tax_due = 29467 + 0.37 * (taxable_income - 120000)
+
+        # Medicare Levy (ML) at 2% of taxable income
+        medicare_levy = taxable_income * 0.02
+
+        if not has_phic and taxable_income > 90000:
+            if taxable_income <= 105000:
+                mls = taxable_income * 0.01
+            elif taxable_income <= 140000:
+                mls = taxable_income * 0.0125
+            else:
+                mls = taxable_income * 0.015
+        else:
+            mls = 0
+
+        # Total tax payable
+        total_tax_payable = tax_due + medicare_levy
+
+        # Calculate tax refund (or additional tax owed)
+        tax_refund = total_tax_withheld - total_tax_payable
+
+        #Calculate Tax estimate
+        tax_estimate = taxable_income - net_income - tax_due - medicare_levy - mls 
+
 
 #Accept RMI
 server1=server1()
